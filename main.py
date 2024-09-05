7335c0cd2bc0e2a5d219bf5ca9a565c7b913aa19
cutebear1989 <104981107+cutebear1989@users.noreply.github.com>
2024-09-05 14:51:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import cv2
 from selenium.webdriver.support.ui import Select
 
 key_names = ["text", "checkbox", "radio", "href", "select"]
